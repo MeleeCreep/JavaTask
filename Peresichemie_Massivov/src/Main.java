7ae71adf56261e9b25552dd430af1abf73a9b6eb
Danila_Harvonen <91787311+MeleeCreep@users.noreply.github.com>
2023-04-16 17:07:40 +0300

    Add files via upload

---
+++ Peresichemie_Massivov/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Введите элементы (через пробел) первого массива:");
+        String fm = input.nextLine();
+        String[] fmsplit = fm.split(" ");
+        System.out.println("Введите элементы (через пробел) второго массива:");
+        String sm = input.nextLine();
+        String[] smsplit = sm.split(" ");
+        ArrayList<String> sameElem = new ArrayList<>();
+        for(int i = 0; i < fmsplit.length; i++){
+            for(int j = 0; j <smsplit.length; j++){
+                if(fmsplit[i].equals(smsplit[j])){
+                    sameElem.add(fmsplit[i]);
+                }
+            }
+        }
+        System.out.printf("%1s", sameElem);
+    }
+}
